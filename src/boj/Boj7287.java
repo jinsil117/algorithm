86252cf64ed30cd0be668fa8dd45c0827f088e26
DESKTOP-H6NGI68\ture9 <jinsil117@nate.com>
2017-10-01 01:37:22 +0900

    Solve NO.7287 from Baekjoon Online Judge

---
+++ src/boj/Boj7287.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 jinsil park
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boj;
+
+/**
+ * Created by jinsil on 2017-10-01
+ * Site       : https://www.acmicpc.net/problem/10172
+ */
+
+
+import java.io.*;
+import java.util.*;
+
+public class Boj7287 {
+    public static void main(String args[]) throws IOException {
+        System.out.println("5");
+        System.out.println("jinsil117");
+    }
+}
+
